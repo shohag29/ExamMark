71d8f7d83f953c9a4b81044b1db08c8a59cda0f0
ShohagHossen <mdshohaghossen29@gmail.com>
2023-09-01 15:41:47 -0700

    Exam Mark

---
+++ script.js
@@ -0,0 +1,22 @@
+function result(Gread) {
+    if (Gread >= 80) {
+        console.log("A+")
+    }
+    else if (Gread >= 70) {
+        console.log("A")
+    }
+    else if (Gread >= 60) {
+        console.log("A-")
+    }
+    else if (Gread >= 50) {
+        console.log("B")
+    }
+    else if (Gread >= 33) {
+        console.log("Pass")
+    }
+    else {
+        console.log(Faild)
+    }
+};
+
+result();
